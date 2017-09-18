959f051e8407642be8833f721ba955fa124bb113
rohith8047 <rohith42008@gmail.com>
2017-09-18 19:55:52 +0530

    connected to db when click

--- client/js/controllers/about.js
+++ client/js/controllers/about.js
@@ -1,6 +1,15 @@
-app.controller('aboutcontroll', ['$scope', function($scope) {
-    $scope.openAlert = function() {
+app.controller('aboutcontroll', ['$scope','$http', function($scope,$http) {
+    $scope.aboutAlert = function() {
         alert('this is about page');
-    };
 
+        $http({
+        	method:'GET',
+        	url:'/api/parallel'
+        }).then(function success(response){
+        	console.log(response);
+        },
+        function error(response){
+        	console.log(response);
+        });
+    }
 }]);
--- client/js/controllers/home.js
+++ client/js/controllers/home.js
@@ -1,7 +1,16 @@
 
-app.controller('homecontroll', ['$scope', function($scope) {
-    $scope.openAlert = function() {
+app.controller('homecontroll', ['$scope','$http', function($scope,$http) {
+    $scope.homeAlert = function() {
         alert('this is home page');
-    };
 
+        $http({
+        	method:'GET',
+        	url:'/api/series'
+        }).then(function success(response){
+        	console.log(response);
+        },
+        function error(response){
+        	console.log(response);
+        });
+    }
 }]);
